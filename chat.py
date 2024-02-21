615649c938f391fb9d02e4eaed1ef8bc3c1f6c45
pavel <11192369+7350206@users.noreply.github.com>
2024-02-21 14:17:09 +0000

    initial

---
+++ chat.py
@@ -0,0 +1,16 @@
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)
+
+# system = "You are an AI assistant that follows instruction extremely well. Help as much as you can. Give short answers."
+# instruction = "Which is the biggest city of India?"
+
+# prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+# for i in llm(prompt, stream=True):
+#     print(i, end="", flush=True)
+# print()
+
+prompt = "what is your dog's"
+print(llm(prompt))
